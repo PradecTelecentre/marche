5827bc91dfe338a4973a26b13346a626588fb19c
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-01-16 00:07:11 +0100

    modifictan des liens

--- resources/views/inc/nav.blade.php
+++ resources/views/inc/nav.blade.php
@@ -8,16 +8,18 @@
         <div class="collapse navbar-collapse header" id="navbarsExampleDefault">
             <ul class="navbar-nav ml-auto item-list p-5">
             <li class="nav-item active">
-                <a class="nav-link" href="/" style="color: black;">ACCUEIL <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="http://goon.mandama.cm/" style="color: black;">ACCUEIL <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item dropdown ">
                 <a class="nav-link dropdown-toggle" style="color: black;" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">SERVICES</a>
                 <div class="dropdown-menu bg-goon " style="border: 0;background: #db8428; text-align: center" aria-labelledby="dropdown01">
-                    <a class="dropdown-item" style="color: black;" href="http://telecentrecameroon.ovh/education">EDUCATION</a>
-                    <a class="dropdown-item" style="color: black;" href="http://telecentrecameroon.ovh/information">INFORMATION</a>
-                    <a class="dropdown-item" style="color: black;" href="#">PARLONS</a>
-                    <a class="dropdown-item" style="color: black;" href="http://localhost:8000">MARCHE</a>
-                    <a class="dropdown-item" style="color: black;" href="#">JEUX</a>
+                    <a class="dropdown-item" style="color: black;" href="http://goon.mandama.cm/education">EDUCATION</a>
+                    <a class="dropdown-item" style="color: black;" href="http://telecentrecameroon.ovh:8080/fr-fr/learn/#/topics">J'APPREND</a>
+                    <a class="dropdown-item" style="color: black;" href="http://goon.mandama.cm/information">LES NOUVELLES</a>
+                    {{-- <a class="dropdown-item" style="color: black;" href="#">PARLONS</a> --}}
+                    <a class="dropdown-item" style="color: black;" href="http://goon.mandama.cm/divertissement">JEUX</a>
+                    <a class="dropdown-item" style="color: black;" href="http://marche.mandama.cm/">le MARCHE</a>
+
                 </div>
             </li>
                 <li class="nav-item">
@@ -36,7 +38,7 @@
         <div class="elementor-shape elementor-shape-bottom orange" data-negative="true">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
                 <path fill="whitesmoke" d="M500,97C126.7,96.3,0.8,19.8,0,0v100l1000,0V1C1000,19.4,873.3,97.8,500,97z"></path>
-            </svg>		
+            </svg>
         </div>
    </div>
 </div>
